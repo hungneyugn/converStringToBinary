51e68d711c22b7c83ddcb25c38333d432a7283cf
Hung <01hungnph01@gmail.com>
2023-03-11 06:30:46 +0700

    update main.c

--- main.c
+++ main.c
@@ -4,20 +4,20 @@
 void converStringToBinary(char* arr)
 {
     int i = 0;
-    int b[10];                            // mang chua ma nhi phan
+    int b[10];                              // mang chua ma nhi phan
     while(i < strlen(arr))
     {
         int j=0;
-        int temp = arr[i];
+        int temp = arr[i];                  // gia tri thap phan cua ki tu
         if(arr[i] == ' ') printf("Space: ");
         else printf("%c: ",arr[i]);
         while(temp > 0)
         {
-            b[j] = temp%2;
+            b[j] = temp%2;                  //chuyen doi thap phan sang nhi phan
             j++;
             temp = temp/2;
         }
-        for(int k = j-1 ; k >= 0; k--)
+        for(int k = j-1 ; k >= 0; k--)      // in ma nhi phan
         {
             printf("%d",b[k]);
         }
@@ -27,7 +27,7 @@ void converStringToBinary(char* arr)
 }
 int main()
 {
-    char arr[]="Hello World";
-    converStringToBinary(arr);
+    char arr[]="Hello World";               // chuoi nhap vao
+    converStringToBinary(arr);              // goi ham chuyen doi
     return 0;
 }
